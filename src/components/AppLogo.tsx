ce76c9fde2e4303b1260ad4a69355894935fd052
mslifelineway <mukeshms40003@gmail.com>
2024-06-27 23:28:48 +0530

    app logo url changed

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -3,14 +3,19 @@ import React from "react";
 
 export const AppLogo = () => {
   return (
-    <Stack direction={"row"} spacing={0.5} alignItems={"center"}>
+    <Stack direction={"row"} spacing={1} alignItems={"center"}>
       <Box className="home__carousel-image-container">
         <Box
           component="img"
-          src={`/assets/images/logo/logo_Brown_transparent.png`}
+          src={`/assets/images/logo/logo_Brown_white.jpeg`}
           alt="logo"
           width={72}
+          height={48}
           mt={1}
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
         />
       </Box>
       <Typography variant="h4" fontSize={"2rem"} fontWeight={700}>
